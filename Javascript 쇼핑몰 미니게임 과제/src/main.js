bfd1c7586336546c4bd92e48eddc17f8bb6d9fd0
axoesnxm <sula9139@gmail.com>
2025-01-01 03:41:35 +0900

    JavaScript - handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -21,11 +21,40 @@ function createHtmlString(item){
     `;
 }
 
+function setEventListener(items){
+    const logo=document.querySelector('.logo');
+    const buttons=document.querySelector('.buttons');
+    logo.addEventListener('click',()=>displayItems(items));
+    buttons.addEventListener('click',()=>onButtonClick(event, items));
+}
+
+function onButtonClick(event,items){
+    const dataset=event.target.dataset;
+    const key=dataset.key;
+    const value=dataset.value;
+
+    if (key==null || value==null){
+        return;
+    }
+    displayItems(items.filter(item=>item[key]===value));
+}
+
+// Make the items matching {keys:value} invisible.
+function updateItems(items,key,value){
+    items.forEach(item=>{
+        if (item.dataset[key]==value){
+            item.classList.remove('invisible');
+        }
+        else{
+            item.classList.add('invisible');
+        }
+    });
+}
+
 //main
 loadItems()
     .then(items=>{
-    console.log(items);
     displayItems(items);
-    //  setEventListener(items)
+    setEventListener(items);
 })
 .catch(console.log);
