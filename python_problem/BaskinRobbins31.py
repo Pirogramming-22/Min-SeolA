1fdfa19c980452b68f610d238f82ab9f9f15ba2a
axoesnxm <sula9139@gmail.com>
2025-01-03 23:35:02 +0900

    3단계

--- python_problem/BaskinRobbins31.py
+++ python_problem/BaskinRobbins31.py
@@ -1,2 +1,10 @@
 num=0
-addNum=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+while True:
+    try:
+        addNum=int(input("부를 숫자의 개수를 입력하세요.(1, 2, 3만 입력 가능) :"))
+        if (addNum<1 or addNum>3):
+            print("1, 2, 3 중 하나를 입력하세요.\n")
+            continue
+    except Exception:
+        print("정수를 입력하세요.\n")
