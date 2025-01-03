30e45fb90ad41e5864c542c68f1cd511dea89a6f
axoesnxm <sula9139@gmail.com>
2025-01-04 00:21:37 +0900

    6단계

--- python_problem/BaskinRobbins31.py
+++ python_problem/BaskinRobbins31.py
@@ -7,6 +7,9 @@ def switchPlayer():
     else:
         return("playerA")
 
+def gameResult(player):
+    print(player, "win!")
+
 while True:
     try:
         addNum=int(input("부를 숫자의 개수를 입력하세요.(1, 2, 3만 입력 가능) :"))
@@ -15,8 +18,14 @@ while True:
             continue
     except Exception:
         print("정수를 입력하세요.\n")
+        continue
     
     for i in range(addNum):
         print(player,":",num+1+i)
     num+=addNum
+    if (num>=31):
+        player=switchPlayer()
+        gameResult(player)
+        break
     player=switchPlayer()
+    
